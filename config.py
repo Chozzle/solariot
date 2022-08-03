deac1623db55d665644eb4358fd244d54fb16f40
carsonholzheimer <carsonholzy@gmail.com>
2022-08-03 12:16:56 +1000

    add config

---
+++ config.py
@@ -0,0 +1,43 @@
+inverter_ip = "192.168.4.45"
+# set inverter_port to 502 for ModbusTCP,
+# If you have a WiNet-S dongle set to 8082 to use HTTP requests
+inverter_port = 502
+# Slave Defaults
+# Sungrow: 0x01
+# SMA: 3
+slave = 0x01
+model = "sungrow-sg5kd"
+timeout = 1
+scan_interval = 5
+
+# Optional:
+#dweepy_uuid = "random-uuid"
+
+# optional
+# prometheus = True
+# prometheus_port = 8000
+
+# Optional:
+# influxdb_ip = "192.168.1.128"
+# influxdb_port = 8086
+# influxdb_user = "user"
+# influxdb_password = "password"
+# influxdb_database = "inverter"
+# influxdb_ssl = True
+# influxdb_verify_ssl = False
+
+# Optional
+#mqtt_server = "192.168.1.128"
+#mqtt_port = 1883
+#mqtt_topic = "inverter/stats"
+#mqtt_username = "user"
+#mqtt_password = "password"
+
+# Optional
+#pvoutput_api = "api_key"
+#pvoutput_sid = "system_id"
+# 60 for regular accounts, 300 for donation accounts
+#pvoutput_rate_limit = 60
+
+# Optional: save telemetry to a json file
+#json_file = "telemetry.json"
